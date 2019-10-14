85160bbba6a1acd68f903768f13ba6145e21faa4
Brendan Rokebrand <brendan@league.co.za>
2019-10-14 14:44:51 +0200

    Updates

---
+++ precache-manifest.1ccde14addaa6c86124fee5fff5479d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8179c9c6c31b2c0594516dd1d5fbb362",
+    "url": "/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "0f685c83f00932eb343b",
+    "url": "/Shopping-Cart/static/css/2.545c6387.chunk.css"
+  },
+  {
+    "revision": "0f685c83f00932eb343b",
+    "url": "/Shopping-Cart/static/js/2.94c0e579.chunk.js"
+  },
+  {
+    "revision": "b62649232c2a645baebf",
+    "url": "/Shopping-Cart/static/js/main.1196af24.chunk.js"
+  },
+  {
+    "revision": "a9b9d83142750f7764c5",
+    "url": "/Shopping-Cart/static/js/runtime-main.836b9328.js"
+  }
+]);
